7401c2ef46acaf70fafb91fd2d9289543ea21406
harry-fruit <isaqueduarte17@gmail.com>
2021-11-11 19:11:20 -0300

    feature: Vacina implementada

--- src/projeto/vacina/ProjetoVacina.java
+++ src/projeto/vacina/ProjetoVacina.java
@@ -4,7 +4,24 @@ public class ProjetoVacina {
 
     public static void main(String[] args) {
         //todo
+    }
+    
+    public static void runPessoas (){
+        Pessoas pessoasDB = new Pessoas();
         
+        pessoasDB.cadastrarPessoa("Isaque", 21, 123456);
+        pessoasDB.cadastrarPessoa("Juliana", 41, 555452);
+        pessoasDB.cadastrarPessoa("Roberto Carlos", 66, 52431);
+        pessoasDB.cadastrarPessoa("Romero Brito", 77, 789456);
+        
+        System.out.println(pessoasDB.consultarPessoa(785555));
+        pessoasDB.atualizarPessoa("Romero Brito", 78, 789456);
+        System.out.println(pessoasDB.consultarPessoa(789456));
+        pessoasDB.deletarPessoa(789456);
+        System.out.println(pessoasDB.consultarPessoa(789456));
+    }
+    
+    public static void runFuncionarios(){
         Funcionarios funcionariosDB = new Funcionarios();
         funcionariosDB.cadastrarPessoa("Roger", 21, 123456);
         funcionariosDB.cadastrarPessoa("Catarina", 41, 555452);
@@ -22,22 +39,20 @@ public class ProjetoVacina {
         
         funcionariosDB.atualizarFuncionario(2, "Enfermeira Chefe");
         funcionariosDB.consultarTodosFuncionarios();
-        
     }
     
-    public static void runPessoas (){
-        Pessoas pessoasDB = new Pessoas();
+    public static void runVacinas(){
+        Vacinas vacinasDB = new Vacinas();
         
-        pessoasDB.cadastrarPessoa("Isaque", 21, 123456);
-        pessoasDB.cadastrarPessoa("Juliana", 41, 555452);
-        pessoasDB.cadastrarPessoa("Roberto Carlos", 66, 52431);
-        pessoasDB.cadastrarPessoa("Romero Brito", 77, 789456);
-        
-        System.out.println(pessoasDB.consultarPessoa(785555));
-        pessoasDB.atualizarPessoa("Romero Brito", 78, 789456);
-        System.out.println(pessoasDB.consultarPessoa(789456));
-        pessoasDB.deletarPessoa(789456);
-        System.out.println(pessoasDB.consultarPessoa(789456));
+        vacinasDB.cadastrarVacina(2, "Pfizer", "ND/A", "30 dias", "Pfizer");
+        vacinasDB.cadastrarVacina(1, "J.J", "ND/A", "30 dias", "Johnsons");
+        vacinasDB.cadastrarVacina(2, "CoronaVac", "ND/A", "30 dias", "Butantã");
+        vacinasDB.cadastrarVacina(2, "AstraZeneca", "ND/A", "30 dias", "Lodres");
+        vacinasDB.consultarVacinas();
+        vacinasDB.atualizarVacina(2, 1, "Johnsons", "ND/A", "20 dias", "Johnsons");
+        vacinasDB.consultarVacina(2);
+        vacinasDB.removerVacina(3);
+        vacinasDB.consultarVacinas();
     }
     
 }
---
+++ src/projeto/vacina/Vacinas.java
@@ -0,0 +1,80 @@
+package projeto.vacina;
+
+import java.util.ArrayList;
+
+public class Vacinas {
+    protected ArrayList<Integer> idVacinas = new ArrayList<Integer>(); 
+    protected ArrayList<Integer> doses = new ArrayList<Integer>();
+    protected ArrayList<String> nomesVacinas = new ArrayList<String>(); 
+    protected ArrayList<String> reacoes = new ArrayList<String>(); 
+    protected ArrayList<String> validades = new ArrayList<String>(); 
+    protected ArrayList<String> fabricantes = new ArrayList<String>(); 
+    
+    public void cadastrarVacina(int doses, String nomeVacina, String reacoes, String validade, String fabricante){ 
+        this.idVacinas.add(this.idVacinas.size() + 1);
+        this.nomesVacinas.add(nomeVacina);
+        this.reacoes.add(reacoes);
+        this.validades.add(validade);
+        this.fabricantes.add(fabricante);
+        this.doses.add(doses);
+        
+        System.out.println("Vacina cadastrada com sucesso.");
+    }
+    
+    public void consultarVacinas(){
+        String message;
+        for(int index = 0; index < this.idVacinas.size(); index++){
+            message = String.format("Id: %s | Nome: %s | Reações: %s | Validade: %s | Fabricante: %s | Doses: %s", this.idVacinas.get(index), this.nomesVacinas.get(index), this.reacoes.get(index), this.validades.get(index), this.fabricantes.get(index), this.doses.get(index));
+            System.out.println(message);
+        }
+    }
+    
+    public void consultarVacina(int idVacina){
+        int index = this.idVacinas.indexOf(idVacina);
+        String message;
+        
+        if (index == -1){
+            System.out.println("Nenhuma vacina corresponde à este ID.");
+            return;
+        }
+        
+        message = String.format("Id: %s | Nome: %s | Reações: %s | Validade: %s | Fabricante: %s | Doses: %s", this.idVacinas.get(index), this.nomesVacinas.get(index), this.reacoes.get(index), this.validades.get(index), this.fabricantes.get(index), this.doses.get(index));
+
+        
+        System.out.println(message);
+    }
+    
+    public void atualizarVacina(int idVacina, int doses, String nome, String reacoes, String validade, String fabricante) {
+        int index = this.idVacinas.indexOf(idVacina);
+        System.out.println(index);
+        
+        if (index == -1){
+            System.out.println("Nenhuma vacina corresponde à este ID.");
+            return;
+        }
+        
+        this.nomesVacinas.set(index, nome);
+        this.doses.set(index, doses);
+        //this.reacoes.set(index, reacoes);
+        //this.fabricantes.set(index, fabricante);
+        //this.validades.set(index, validade);
+
+        System.out.println("Vacina atualizada com sucesso.");
+    }
+    
+    public void removerVacina(int idVacina){
+        int index = this.idVacinas.indexOf(idVacina);
+        
+        if (index == -1){
+            System.out.println("Nenhuma vacina corresponde à este ID.");
+            return;
+        }
+        this.idVacinas.remove(index);
+        this.nomesVacinas.remove(index);
+        this.doses.remove(index);
+        this.reacoes.remove(index);
+        this.fabricantes.remove(index);
+        this.validades.remove(index);
+        System.out.println("Vacina removida com sucesso.");
+    }
+}
